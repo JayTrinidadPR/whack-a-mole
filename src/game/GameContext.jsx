5d7880dcfa331097ff38c9c22c5c847c7381ddc4
jaytrinidadpr <justinquelix@icloud.com>
2026-02-05 20:29:36 -0600

    finished 1.0

--- src/game/GameContext.jsx
+++ src/game/GameContext.jsx
@@ -1,53 +1,51 @@
-import { useState, createContext, useContext, useMemo } from "react";
+import { createContext, useContext, useMemo, useState } from "react";
 
 const GameContext = createContext(null);
 
-const TIME_LIMIT = 15;
-
 function getRandomHoleIndex(holeCount) {
-    return Math.floor(Math.random() * holeCount);
+  return Math.floor(Math.random() * holeCount);
 }
 
 export function GameProvider({ children }) {
-    const HOLE_COUNT = 9;
-}
+  const HOLE_COUNT = 9;
 
-const [phase, setPhase] = useState("welcome");
-const [score, setScore] useState(0);
+  const [phase, setPhase] = useState("welcome"); // "welcome" | "playing"
+  const [score, setScore] = useState(0);
+  const [moleIndex, setMoleIndex] = useState(() => getRandomHoleIndex(HOLE_COUNT));
 
-const [moleIndex, setMoleIndex] = useState(() => getRandomHoleIndex(HOLE_COUNT));
-
-function startGame() {
+  function startGame() {
     setScore(0);
     setMoleIndex(getRandomHoleIndex(HOLE_COUNT));
     setPhase("playing");
-}
+  }
 
-function restartGame() {
+  function restartGame() {
     setPhase("welcome");
-}
-function whackMole() {
+  }
+
+  function whackMole() {
     setScore((s) => s + 1);
     setMoleIndex(getRandomHoleIndex(HOLE_COUNT));
-}
+  }
 
-const value = useMemo(
+  const value = useMemo(
     () => ({
-        HOLE_COUNT,
-        phase,
-        score,
-        moleIndex,
-        startGame,
-        restartGame,
-        whackMole
+      HOLE_COUNT,
+      phase,
+      score,
+      moleIndex,
+      startGame,
+      restartGame,
+      whackMole,
     }),
     [phase, score, moleIndex]
-);
+  );
 
-return <GameContext.Provider value={value}>{children} </GameContext.Provider>;
+  return <GameContext.Provider value={value}>{children}</GameContext.Provider>;
+}
 
 export function useGame() {
-    const ctx = useContext(GameContext);
-    if (!ctx) throw new Error("ITS NOT WORKING, useGame must be inside" < GameProvider);
-    return ctx;
+  const ctx = useContext(GameContext);
+  if (!ctx) throw new Error("useGame must be used inside <GameProvider>");
+  return ctx;
 }
